7552b6f758b87e551d7d984059c6c5cd63010f8e
Raj Prakash Maurya <raj.maurya@bksgold.com>
2023-07-18 15:24:11 +0530

    slider

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,20 @@
+
+import Slider from '@mui/material/Slider';
 import React from "react";
+import Stack from '@mui/material/Stack';
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
 
 function Login() {
     return(
         <div>
             <h2>login</h2>
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+            <VolumeDown />
+            <Slider aria-label="Volume"   />
+                <VolumeUp />
+                </Stack>
+                <Slider disabled defaultValue={30} aria-label="Disabled slider" />
         </div>
     )
 }
